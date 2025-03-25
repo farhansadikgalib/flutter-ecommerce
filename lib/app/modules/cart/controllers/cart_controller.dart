ba1a96081118f135665a78c945d58a0f398e06dc
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-03-25 13:19:12 +0600

    Refactor CartController to extend BaseController and add cart filtering logic

--- lib/app/modules/cart/controllers/cart_controller.dart
+++ lib/app/modules/cart/controllers/cart_controller.dart
@@ -1,20 +1,38 @@
 import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:turi/app/core/base/base_controller.dart';
+import 'package:turi/app/core/helper/print_log.dart';
+import 'package:turi/app/modules/home/controllers/home_controller.dart';
 
-class CartController extends GetxController {
+class CartController extends BaseController {
 
   @override
   void onInit() {
     super.onInit();
   }
 
+
   @override
   void onReady() {
     super.onReady();
+    cartFilter();
   }
 
-  @override
-  void onClose() {
-    super.onClose();
+  cartFilter() {
+
+    Get.find<HomeController>().homeElements.first.collections!.forEach(
+      (element) {
+        element.productCollections!.forEach((product) {
+
+          if (product.addToCart == true) {
+            printLog(product.title);
+          }
+
+        });
+      }
+    );
   }
+
+
 
 }
