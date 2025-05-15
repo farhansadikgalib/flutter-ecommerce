72591d8279ed25aff93a64f5642386cee84939cd
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-05-15 18:14:44 +0600

    Add product review fetching logic to ProductDetailsController

--- lib/app/modules/product_details/controllers/product_details_controller.dart
+++ lib/app/modules/product_details/controllers/product_details_controller.dart
@@ -7,35 +7,63 @@ import 'package:turi/app/data/remote/model/home/home_response.dart';
 import 'package:turi/app/data/remote/model/product/product_details_response.dart';
 import 'package:turi/app/data/remote/repository/product/product_repository.dart';
 
+import '../../../data/remote/model/product/product_review_response.dart';
+
 class ProductDetailsController extends BaseController {
   final PageController pageController = PageController();
   final currentPage = 0.obs;
   ProductCollection product = Get.arguments['product'];
 
   final productDetails = <ProductDetails>[].obs;
+  final imageList = <ProductImage>[].obs;
+  final productReview = <ProductReview>[].obs;
 
   @override
   void onInit() {
     super.onInit();
-    printLog(product.title);
-    printLog(product.id);
-    getProductDetails();
 
+    getProductDetails();
+    getProductReview();
   }
 
-
-  getProductDetails()async{
-
-    var response = await ProductRepository().getProductDetails(product.id.toString());
-    if(response.status==200){
+  getProductDetails() async {
+    var response = await ProductRepository().getProductDetails(
+      product.id.toString(),
+    );
+    if (response.status == 200) {
       productDetails.clear();
+      imageList.clear();
       productDetails.add(response.data!);
-    }else{
+      imageList.add(
+        ProductImage(
+          id: 0,
+          image: productDetails.first.image,
+          productId: product.id,
+          createdAt: '',
+          updatedAt: '',
+        ),
+      );
+      imageList.addAll(productDetails.first.images!);
+
+
+      printLog(imageList.length);
+    } else {
       printLog(response.message);
       AppWidgets().getSnackBar(message: response.message.toString());
     }
   }
 
+  void getProductReview()async {
+    var response = await ProductRepository().getProductReview(
+      product.id.toString(),
+    );
 
+    if(response.status == 200) {
+   //   productReview.addAll(response.data! as Iterable<ProductReview>);
+    } else {
+      printLog(response.message);
+      AppWidgets().getSnackBar(message: response.message.toString());
+    }
 
+  }
 }
