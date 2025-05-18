debb294d53f495a663ea7236e8324486c5cc92aa
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-05-18 11:49:58 +0600

    Add shipping ID and price range parameters to category product fetching

--- lib/app/data/remote/repository/category/category_repository.dart
+++ lib/app/data/remote/repository/category/category_repository.dart
@@ -2,13 +2,20 @@ import 'package:turi/app/data/remote/model/category/categorywiseproducts_respons
 import '../../../../network_service/api_client.dart';
 import '../../../../network_service/api_end_points.dart';
 
-class CategoryRepository{
-  Future<CategoryWiseProductsResponse> getCategoryWiseProduct(String
-  categorySlug,
-      String brandId)
-  async {
+class CategoryRepository {
+  Future<CategoryWiseProductsResponse> getCategoryWiseProduct(
+    String categorySlug,
+    String brandId,
+    String shippingId,
+      String minPrice,
+      String maxPrice,
+  ) async {
     var response = await ApiClient().get(
-      ApiEndPoints.categoryList(productCategory: categorySlug,brandId:brandId ),
+      ApiEndPoints.categoryList(
+        productCategory: categorySlug,
+        brandId: brandId,
+        shippingId: shippingId, maxPrice: minPrice, minPrice: maxPrice,
+      ),
       getCategoryWiseProduct,
       isHeaderRequired: false,
       isLoaderRequired: false,
@@ -16,5 +23,4 @@ class CategoryRepository{
 
     return categoryWiseProductsResponseFromJson(response.toString());
   }
-
 }
