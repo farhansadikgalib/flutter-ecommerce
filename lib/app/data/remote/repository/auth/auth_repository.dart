c38034d2a93f0ade50b579c9b294fe17bdb4de8b
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-06-22 15:56:32 +0600

    Add authentication methods for signup, OTP verification, password reset, and update

--- lib/app/data/remote/repository/auth/auth_repository.dart
+++ lib/app/data/remote/repository/auth/auth_repository.dart
@@ -1,3 +1,7 @@
+import 'package:turi/app/data/remote/model/auth/forget_password_response.dart';
+import 'package:turi/app/data/remote/model/auth/otp_verify_response.dart';
+import 'package:turi/app/data/remote/model/auth/sign_up_response.dart';
+import 'package:turi/app/data/remote/model/auth/update_password_response.dart';
 
 import '../../../../network_service/api_client.dart';
 import '../../../../network_service/api_end_points.dart';
@@ -5,18 +9,11 @@ import '../../model/auth/login_response.dart';
 import '../../model/auth/logout_response.dart';
 
 class AuthRepository {
-
-  Future<LoginResponse> getUserLogin(
-    String staffId,
-    String password,
-  ) async {
+  Future<LoginResponse> login(String email, String password) async {
     var response = await ApiClient().post(
       ApiEndPoints.login,
-      {
-        "staffid": staffId,
-        "password": password,
-      },
-      getUserLogin,
+      {"email": email, "password": password},
+      login,
       isHeaderRequired: false,
       isLoaderRequired: true,
     );
@@ -24,19 +21,77 @@ class AuthRepository {
     return loginResponseFromJson(response.toString());
   }
 
+  Future<SignUpResponse> signup(
+    String name,
+    String email,
+    String password,
+  ) async {
+    var response = await ApiClient().post(
+      ApiEndPoints.signup,
+      {"name": name, "email": email, "password": password},
+      signup,
+      isHeaderRequired: false,
+      isLoaderRequired: true,
+    );
+
+    return signUpResponseFromJson(response.toString());
+  }
+
+  Future<OtpVerifyResponse> verifyOTP(
+      String email,
+      String code,
+      ) async {
+    var response = await ApiClient().post(
+      ApiEndPoints.verifyOtp,
+      { "email": email, "code": code},
+      verifyOTP,
+      isHeaderRequired: false,
+      isLoaderRequired: true,
+    );
+
+    return otpVerifyResponseFromJson(response.toString());
+  }
+
+
+
+  Future<ForgetPasswordResponse> forgetPassword(String email) async {
+    var response = await ApiClient().post(
+      ApiEndPoints.forgetPassword,
+      {"email": email},
+      forgetPassword,
+      isHeaderRequired: false,
+      isLoaderRequired: true,
+    );
+
+    return forgetPasswordResponseFromJson(response.toString());
+  }
+
+  Future<UpdatePasswordResponse> updatePassword(
+      String code,
+      String email,
+      String password,
+      ) async {
+    var response = await ApiClient().post(
+      ApiEndPoints.updatePassword,
+      {"code": code, "email": email, "password": password},
+      updatePassword,
+      isHeaderRequired: false,
+      isLoaderRequired: true,
+    );
+
+    return updatePasswordResponseFromJson(response.toString());
+  }
+
 
   Future<LogoutResponse> getUserLogOut() async {
     var response = await ApiClient().post(
       ApiEndPoints.logout,
-      {
-      },
-      getUserLogin,
+      {},
+      getUserLogOut,
       isHeaderRequired: true,
       isLoaderRequired: true,
     );
 
     return logoutResponseFromJson(response.toString());
   }
-
-
 }
