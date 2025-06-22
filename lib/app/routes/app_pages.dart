d146ad73a6f1e47ddd15440ab69fdb1f23a80640
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-06-22 15:56:06 +0600

    Add login route and view to app navigation

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,4 @@
 import 'package:get/get.dart';
-
-import '../modules/auth/bindings/auth_binding.dart';
-import '../modules/auth/views/auth_view.dart';
 import '../modules/cart/bindings/cart_binding.dart';
 import '../modules/cart/views/cart_view.dart';
 import '../modules/checkout/bindings/checkout_binding.dart';
@@ -10,6 +7,8 @@ import '../modules/dashboard/bindings/dashboard_binding.dart';
 import '../modules/dashboard/views/dashboard_view.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
+import '../modules/login/bindings/login_binding.dart';
+import '../modules/login/views/login_view.dart';
 import '../modules/product_category/bindings/product_category_binding.dart';
 import '../modules/product_category/views/product_category_view.dart';
 import '../modules/product_details/bindings/product_details_binding.dart';
@@ -30,11 +29,7 @@ class AppPages {
       page: () => HomeView(),
       binding: HomeBinding(),
     ),
-    GetPage(
-      name: _Paths.AUTH,
-      page: () => const AuthView(),
-      binding: AuthBinding(),
-    ),
+
     GetPage(
       name: _Paths.DASHBOARD,
       page: () => const DashboardView(),
@@ -64,6 +59,11 @@ class AppPages {
       name: _Paths.SPLASH,
       page: () => const SplashView(),
       binding: SplashBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => const LoginView(),
+      binding: LoginBinding(),
     ),
   ];
 }
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -3,23 +3,23 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
-  static const AUTH = _Paths.AUTH;
   static const DASHBOARD = _Paths.DASHBOARD;
   static const CART = _Paths.CART;
   static const CHECKOUT = _Paths.CHECKOUT;
   static const PRODUCT_DETAILS = _Paths.PRODUCT_DETAILS;
   static const PRODUCT_CATEGORY = _Paths.PRODUCT_CATEGORY;
   static const SPLASH = _Paths.SPLASH;
+  static const LOGIN = _Paths.LOGIN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
-  static const AUTH = '/auth';
   static const DASHBOARD = '/dashboard';
   static const CART = '/cart';
   static const CHECKOUT = '/checkout';
   static const PRODUCT_DETAILS = '/product-details';
   static const PRODUCT_CATEGORY = '/product-category';
   static const SPLASH = '/splash';
+  static const LOGIN = '/login';
 }
