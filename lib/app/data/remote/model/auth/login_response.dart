5b9af9cefb58613eaae8e1e57673cf83d648c271
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-06-22 15:57:14 +0600

    Refactor LoginResponse model to include status, token, and message fields; update Admin class structure

--- lib/app/data/remote/model/auth/login_response.dart
+++ lib/app/data/remote/model/auth/login_response.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 
 LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.decode(str));
@@ -6,49 +5,97 @@ LoginResponse loginResponseFromJson(String str) => LoginResponse.fromJson(json.d
 String loginResponseToJson(LoginResponse data) => json.encode(data.toJson());
 
 class LoginResponse {
-  int? id;
-  String? staffid;
-  String? name;
-  String? email;
-  String? role;
-  String? designation;
-  String? token;
-  String? message;
+  Data? data;
   int? status;
+  dynamic token;
+  String? message;
 
   LoginResponse({
-    this.id,
-    this.staffid,
-    this.name,
-    this.email,
-    this.role,
-    this.designation,
+    this.data,
+    this.status,
     this.token,
     this.message,
-    this.status,
   });
 
   factory LoginResponse.fromJson(Map<String, dynamic> json) => LoginResponse(
-    id: json["id"],
-    staffid: json["staffid"],
-    name: json["name"],
-    email: json["email"],
-    role: json["role"],
-    designation: json["designation"],
+    data: json["data"] == null ? null : Data.fromJson(json["data"]),
+    status: json["status"],
     token: json["token"],
     message: json["message"],
-    status: json["status"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "data": data?.toJson(),
+    "status": status,
+    "token": token,
+    "message": message,
+  };
+}
+
+class Data {
+  String? expiresIn;
+  String? token;
+  Admin? admin;
+
+  Data({
+    this.expiresIn,
+    this.token,
+    this.admin,
+  });
+
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+    expiresIn: json["expires_in"],
+    token: json["token"],
+    admin: json["admin"] == null ? null : Admin.fromJson(json["admin"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "expires_in": expiresIn,
+    "token": token,
+    "admin": admin?.toJson(),
+  };
+}
+
+class Admin {
+  int? id;
+  String? name;
+  String? username;
+  String? email;
+  dynamic code;
+  dynamic commission;
+  String? createdAt;
+  String? updatedAt;
+
+  Admin({
+    this.id,
+    this.name,
+    this.username,
+    this.email,
+    this.code,
+    this.commission,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  factory Admin.fromJson(Map<String, dynamic> json) => Admin(
+    id: json["id"],
+    name: json["name"],
+    username: json["username"],
+    email: json["email"],
+    code: json["code"],
+    commission: json["commission"],
+    createdAt: json["created_at"],
+    updatedAt: json["updated_at"],
   );
 
   Map<String, dynamic> toJson() => {
     "id": id,
-    "staffid": staffid,
     "name": name,
+    "username": username,
     "email": email,
-    "role": role,
-    "designation": designation,
-    "token": token,
-    "message": message,
-    "status": status,
+    "code": code,
+    "commission": commission,
+    "created_at": createdAt,
+    "updated_at": updatedAt,
   };
 }
