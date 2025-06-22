d9aa3577b51250bc4f673dd5f524e73ef88cd482
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-06-22 15:55:55 +0600

    Update API endpoints for user authentication and password management

--- lib/app/network_service/api_end_points.dart
+++ lib/app/network_service/api_end_points.dart
@@ -2,7 +2,11 @@
 class ApiEndPoints {
 
   //Authentication
-  static String login = "login";
+  static String login = "user/signin";
+  static String signup = "user/signup";
+  static String verifyOtp = "user/verify";
+  static String forgetPassword = "user/forgot-password";
+  static String updatePassword = "user/update-password";
   static String logout = "logout";
   //Authentication
 
