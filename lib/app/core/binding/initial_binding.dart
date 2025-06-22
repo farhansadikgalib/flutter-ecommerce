37c33a35ce930c725b3960eb7944aee2ee078ba6
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-06-22 15:57:02 +0600

    Add controllers for home, cart, and login in initial binding

--- lib/app/core/binding/initial_binding.dart
+++ lib/app/core/binding/initial_binding.dart
@@ -1,12 +1,17 @@
 import 'package:get/get.dart';
+import '../../modules/cart/controllers/cart_controller.dart';
+import '../../modules/home/controllers/home_controller.dart';
+import '../../modules/login/controllers/login_controller.dart';
 import '../connection_manager/connection_manager_binding.dart';
 
 class InitialBinding extends Bindings {
   @override
   void dependencies() {
     ConnectionManagerBinding().dependencies();
-    // AirportSearchBinding().dependencies();
-    // CalenderPickerBinding().dependencies();
-    // HomeBinding().dependencies();
+
+    Get.put(HomeController(), permanent: true);
+    Get.put(CartController(), permanent: true);
+
+    Get.lazyPut(() => LoginController());
   }
 }
