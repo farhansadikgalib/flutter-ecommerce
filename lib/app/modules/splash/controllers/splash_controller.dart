b80dd2e78da160db2d6085be716a17b5ecd38a1e
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-06-23 10:37:18 +0600

    Refactor SplashController to improve app initialization and load items using AuthHelper

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,15 +1,14 @@
 import 'package:get/get.dart';
+import 'package:turi/app/core/helper/app_helper.dart';
+import 'package:turi/app/core/helper/auth_helper.dart';
 
 import '../../../routes/app_pages.dart';
 
 class SplashController extends GetxController {
-
-  initializeApp() async {
+  Future<void> initializeApp() async {
+    AuthHelper().loadItems();
     await Future.delayed(2500.milliseconds).then((value) async {
-          Get.offNamed(
-            Routes.DASHBOARD,
-          );
-
+      Get.offNamed(Routes.DASHBOARD);
     });
   }
 }
