2d4d965b1262934ae292580334d080dcb6329f06
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-07-02 11:37:45 +0600

    Update AppConfig to use empty fallback for version and release date

--- lib/app/core/config/app_config.dart
+++ lib/app/core/config/app_config.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
-  static String get currentVersion => dotenv.get('CURRENT_VERSION', fallback: '1.0.0');
+  static String get currentVersion => dotenv.get('CURRENT_VERSION', fallback: '');
   static bool get https => dotenv.get('HTTPS', fallback: 'true') == 'true';
   static String get publicFolder => dotenv.get('PUBLIC_FOLDER', fallback: 'public');
   static String get protocol => https ? "https://" : "http://";
@@ -10,5 +10,5 @@ class AppConfig {
   static String get rawBaseUrl => "$protocol$domainPath";
   static String get basePath => "$rawBaseUrl/$apiEndPath";
   static String get imageBasePath => dotenv.get('IMAGE_BASE_PATH', fallback: '');
-  static String get releaseDate => dotenv.get('RELEASE_DATE', fallback: '050125');
+  static String get releaseDate => dotenv.get('RELEASE_DATE', fallback: '');
 }
