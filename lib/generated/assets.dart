d21de07feb442eaba0b1eb9ecea0a275711f66d3
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-09-14 09:53:15 +0600

    Add not found image asset: include 'not_found.png' in assets

--- lib/generated/assets.dart
+++ lib/generated/assets.dart
@@ -6,6 +6,7 @@ class Assets {
   static const String pngB2 = 'assets/png/b2.png';
   static const String pngB3 = 'assets/png/b3.png';
   static const String pngLogo = 'assets/png/logo.png';
+  static const String pngNotFound = 'assets/png/not_found.png';
   static const String turi = '.env';
 
 }
