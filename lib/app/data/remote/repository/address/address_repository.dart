2b4bacc566887a8ec73c401682e06c4fcefd9736
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2025-09-22 18:12:08 +0600

    Add address management methods: implement delete, set default, and create address functionalities

--- lib/app/data/remote/repository/address/address_repository.dart
+++ lib/app/data/remote/repository/address/address_repository.dart
@@ -1,3 +1,4 @@
+import 'package:ousadbazar/app/data/remote/model/address/default_response.dart';
 import 'package:ousadbazar/app/data/remote/model/address/shipping_address_response.dart';
 import '../../../../services/network_service/api_client.dart';
 import '../../../../services/network_service/api_end_points.dart';
@@ -13,5 +14,36 @@ class AddressRepository {
     return addressResponseFromJson(response.toString());
   }
 
+  Future<DefaultResponse> deleteAddress(String id) async {
+    var response = await ApiClient().delete(
+      ApiEndPoints.deleteCustomerAddress(id: id),
+      {},
+      deleteAddress,
+      isHeaderRequired: true,
+      isLoaderRequired: true,
+    );
+    return defaultResponseFromJson(response.toString());
+  }
 
+  Future<DefaultResponse> defaultAddress(String id) async {
+    var response = await ApiClient().post(
+      ApiEndPoints.defaultCustomerAddress(id: id),
+      {},
+      defaultAddress,
+      isHeaderRequired: true,
+      isLoaderRequired: true,
+    );
+    return defaultResponseFromJson(response.toString());
+  }
+
+  Future<DefaultResponse> createAddress(body) async {
+    var response = await ApiClient().post(
+      ApiEndPoints.addCustomerAddress,
+      body,
+      createAddress,
+      isHeaderRequired: true,
+      isLoaderRequired: true,
+    );
+    return defaultResponseFromJson(response.toString());
+  }
 }
